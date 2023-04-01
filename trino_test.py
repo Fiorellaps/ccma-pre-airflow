9d22f9df1a073e4bfa5cc3abf9a0f8990088fd0e
Fiorella Piriz Sapio <61502415+Fiorellaps@users.noreply.github.com>
2023-04-01 15:10:25 +0200

    Create trino_test.py

---
+++ trino_test.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.trino.operators.trino import TrinoOperator
+from trino.dbapi import trino
+
+
+def query_trino(**kwargs):
+    conn = trino.dbapi.connect(
+            host='trino.apps.k8spro.nextret.net',
+            port=31858,
+            catalog="hive",
+            user='admin')
+    query = "SHOW catalogs"    
+    cur = conn.cursor()
+    cur.execute(query)
+    rows = cur.fetchall()
+    print(rows)
+
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 4, 1),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'query_trino',
+    default_args=default_args,
+    catchup=False,
+    schedule_interval='@daily',
+    tags=['trino', 'test']
+) as dag:
+  
+  trino_query = TrinoOperator(
+        task_id="trino_templated_query",
+        sql="SHOW CATALOGS",
+        handler=list
+    )
+    '''t1 = PythonOperator(
+        task_id='run_query',
+        python_callable=query_trino,
+    )'''
+
+trino_query
