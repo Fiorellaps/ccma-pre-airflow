ff26556e286854bd9abb6db934da5c81c9f8f0e8
Fiorella Piriz Sapio <61502415+Fiorellaps@users.noreply.github.com>
2023-04-01 14:49:08 +0200

    Update test_s3.py

--- test_s3.py
+++ test_s3.py
@@ -5,6 +5,7 @@ from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 
 
 def load_data_from_s3(**kwargs):
+    '''
     s3_hook = S3Hook(aws_conn_id='cmma-pre-s3')
     bucket_name = 'ccma-pre'
     s3_key = 'prova.txt'
@@ -13,6 +14,29 @@ def load_data_from_s3(**kwargs):
     keys = s3_hook.list_keys(bucket_name)
     for key in keys:
         print(key)
+     '''
+    {
+   "region_name":"us-east-1",
+   "aws_access_key_id":"ZZ0JLR12PPW4410IW3G9",
+   "aws_secret_access_key":"yBUSPjz6OxKcIChDGQ0Cd1I7o9Av4bZYZJYT3CJJ",
+   "endpoint_url":"http://rook-ceph-rgw-my-store-rook-ceph.apps.k8spro.nextret.net",
+   "use_ssl":false,
+   "verify":false
+}
+    s3_client = boto3.client(
+        "s3",
+        "us-east-1",
+        aws_access_key_id= "ZZ0JLR12PPW4410IW3G9",
+        aws_secret_access_key= "yBUSPjz6OxKcIChDGQ0Cd1I7o9Av4bZYZJYT3CJJ",
+        endpoint_url= "http://rook-ceph-rgw-my-store-rook-ceph.apps.k8spro.nextret.net",
+        use_ssl= False,
+        verify= False
+    )
+
+    my_bucket = s3.Bucket('ccma-pre')
+
+    for file in my_bucket.objects.all():
+        print(file.key)
 
 
 default_args = {
