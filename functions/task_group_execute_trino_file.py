a1a252d03d0396926b00d86a3815ccf85765e5fd
FIORELLAPS <fiorellapiriz@hotmail.com>
2023-04-19 17:31:13 +0200

    update trino query

--- functions/task_group_execute_trino_file.py
+++ functions/task_group_execute_trino_file.py
@@ -5,7 +5,7 @@ from airflow import DAG
 import sys
 sys.path.insert(0, '/opt/bitnami/airflow/dags/git_dags/')
 sys.path.insert(0,'/opt/bitnami/airflow/dags/git_dags/functions')
-from functions.task_execute_trino_query import execute_trino_query
+#from functions.task_execute_trino_query import execute_trino_query
 from functions.utiles.s3_utiles import read_data_from_s3
 
 
@@ -19,15 +19,15 @@ def execute_trino_file(dag: DAG, config):
                 if query:
                     print("--query", query)
                     task_id = "execute_trino_query_" + str(i)
-                    execute_trino_query = TrinoOperator(
+                    task = TrinoOperator(
                                             task_id=task_id,
                                             sql=query,
                                             handler=list,
                                             dag=dag
                                         )
-                    taskgroup.add(execute_trino_query)
+                    taskgroup.add(task)
 
-    task_group_id =  "execute_queries_" + config['query_name']
+    task_group_id =  "execute_" + config['query_name']
     with TaskGroup(
         task_group_id, tooltip="Tarea para ejecutar las queries de un fichero sql"
     ) as taskgroup:
@@ -37,7 +37,7 @@ def execute_trino_file(dag: DAG, config):
                             file_path=config['query_file_path']
                             )
         
-        process_trino_query(dag, taskgroup, query )
+        process_trino_query(dag, taskgroup, query)
        
 
     return taskgroup
