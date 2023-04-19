110ac462169db92ab5568085696218844fb7c6ef
FIORELLAPS <fiorellapiriz@hotmail.com>
2023-04-19 16:21:31 +0200

    update

--- dags/dag_gfk.py
+++ dags/dag_gfk.py
@@ -55,6 +55,7 @@ with DAG(
         "code_type": "Java", # Java, R or Python
         "application_s3_location": "s3a://airflowdags/gfk/ccma-etl-0.2314.0-SNAPSHOT-jar-with-dependencies.jar",
         "application_main_class": "com.pragsis.ccma.etl.control.ControlProcess",
+        "application_arguments": ["gfk_pgfk_csv"]
     }
 
     spark_application_gfk_pgfk_csv = execute_spark_application(
